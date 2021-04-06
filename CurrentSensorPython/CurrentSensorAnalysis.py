e79e8f277e29d6f8c24b2f5a810c1308326058ce
IonicRob <62338052+IonicRob@users.noreply.github.com>
2021-04-06 16:05:37 +0100

    Update CurrentSensorAnalysis.py
    Added it so it could analyse the old format of time and for the current format. Along with acquisition frequency added to the titles and code.
    Also changed it so it uses Pandas rather than the previous clunky method.

--- CurrentSensorPython/CurrentSensorAnalysis.py
+++ CurrentSensorPython/CurrentSensorAnalysis.py
@@ -6,8 +6,7 @@
 import matplotlib.pyplot as plt
 import numpy
 import easygui
-import csv
-from collections import defaultdict
+import pandas as pd  # Ths is used for accessing the CSV file after creation for data analysis.
 
 
 def moving_average(x_input, w):
@@ -24,51 +23,57 @@ print('Select mode...')
 TF_IV = easygui.buttonbox('Choose analysis mode:', 'Analysis Mode', ['I vs t', 'I vs V'])
 print(TF_IV)
 
-headers = ("Time (m:s:mu_s):", "Bus Voltage (V):", "Shunt Voltage (mV):", "Load Voltage (V):", "Current (mA):",
+headers = ("Time (s):", "Bus Voltage (V):", "Shunt Voltage (mV):", "Load Voltage (V):", "Current (mA):",
            "Power (mW):")
 
-columns = defaultdict(list)  # each value in each column is appended to a list
+# columns = defaultdict(list)  # each value in each column is appended to a list
 
 print('Main part...')
-with open(files) as f:
-    reader = csv.DictReader(f)  # read rows into a dictionary format
-    for row in reader:  # read a row as {column1: value1, column2: value2,...}
-        for (k, v) in row.items():  # go over each column name and value
-            columns[k].append(float(v))  # append the value into the appropriate list based on column name k
-f.close()
+# with open(files) as f:
+#     reader = csv.DictReader(f)  # read rows into a dictionary format
+#     for row in reader:  # read a row as {column1: value1, column2: value2,...}
+#         for (k, v) in row.items():  # go over each column name and value
+#             columns[k].append(float(v))  # append the value into the appropriate list based on column name k
+# f.close()
 
-if TF_IV is None:
-    print(f'Value of TF_IV = {TF_IV}')
+
+# Using Pandas, the data from the CSV file is read.
+data = pd.read_csv(files)
+
+# In all methods cases, the y-axis is current, and we need the time values to calculate the average acquisition rate.
+y_values = data['Current (mA):']
+
+try:
+    time_values = data['Time (s):']
+except Exception as ex:
+    print(ex)
+    print("File used old header!")
+    time_values = data['Time (m:s:mu_s):']
+
+# Below calculates the average mean acquisition frequency.
+time_difference = [time_values[i + 1] - time_values[i] for i in range(len(time_values)-1)]
+mean_frequency = 1/(sum(time_difference) / len(time_difference))
+
+# The following is just plot aesthetics and changing the x-axis variable.
+if TF_IV == 'I vs V':
+    x_values = data['Load Voltage (V):']
+    plt.xlabel('Load Voltage (V)')
+    window = 5
+    y_values = list(moving_average(y_values, w=window))
+    plt.title(f"Current vs Voltage (window = {window} & f={round(mean_frequency, 2)} Hz)")
 else:
-    if TF_IV == 'I vs t':
-        # In "I vs t" mode
-        x: list = columns["Time (m:s:mu_s):"]
-        y: list = columns["Current (mA):"]
-        x_label = 'Time (s)'
-        y_label = 'Current (mA)'
-        title = "Current vs Time"
-    else:
-        # In "I vs V" mode
-        x: list = columns["Load Voltage (V):"]
-        y: list = columns["Current (mA):"]
-        window = 5
-        y_2 = list(moving_average(y, w=window))
-        if len(y_2) == len(y):
-            y = y_2
-        else:
-            print(f"Length of moving average y is different to x length")
-        x_label = 'Load Voltage (V):'
-        y_label = 'Current (mA)'
-        title = f"Current vs Time (window = {window})"
+    x_values = data['Time (s):']
+    plt.xlabel('Time (s)')
+    plt.title(f"Current vs Time (f={mean_frequency})")
 
-    Number_of_Points = len(x)
-    Colors = list(range(0, Number_of_Points))
-    plt.scatter(x, y, c=Colors, s=6, cmap='cool')  # , marker='x', markerfacecolor='red', markersize=12
-    plt.xlabel(x_label)  # naming the x axis
-    plt.ylabel(y_label)  # naming the y axis
-    plt.title(title)  # giving a title to my graph
-    color_bar = plt.colorbar()
-    color_bar.set_label('Time of Recording (s)', loc='center')
 
-    plt.show()  # function to show the plot
+Number_of_Points = len(x_values)
+Colors = list(range(0, Number_of_Points))
+plt.scatter(x_values, y_values, c=Colors, s=6, cmap='cool')  # , marker='x', markerfacecolor='red', markersize=12
+color_bar = plt.colorbar()
+color_bar.set_label('Time of Recording (s)', loc='center')
+
+print("Code Complete!")
+
+plt.show()  # function to show the plot
 
