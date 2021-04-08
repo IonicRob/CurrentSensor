26a8b05e911ed5dc8e04f801013db7c43622338d
IonicRob <62338052+IonicRob@users.noreply.github.com>
2021-04-08 16:46:13 +0100

    Main Update
    Made a complete version based off the front lab PC.

--- CurrentSensorPython/CurrentSensorCombo.py
+++ CurrentSensorPython/CurrentSensorCombo.py
@@ -6,6 +6,12 @@
 # changed by the user.
 #
 #
+# Note. To run successfully from the PyCharm IDE:
+#   Python interpreter Version = Python 3.9
+#   Yes to "Add content roots to PYTHONPATH" and "Add source roots to PYTHONPATH.
+#   No to all of the "Execution"
+#
+#
 # The user settings in this code has been designed to be limited as much as possible. Currently, the only inputs
 # required by the user is to give the session a name for identification, the live plot refresh rate, and whether they
 # want to do a "current vs time" or a "current vs voltage" live plot on the screen.
@@ -36,39 +42,51 @@ import easygui  # This how I achieve a GUI for the "I vs t" or "I vs V" plotting
 import pandas as pd  # Ths is used for accessing the CSV file after creation for data analysis.
 import serial.tools.list_ports as tool_port
 from warnings import warn
+import keyboard
+from pathlib import Path
 
 
 # Free to Change Settings
 session_name = "Log Current Combo"  # This is the ID for your recording session. Extra details are auto added.
-live_plot_refresh_time = 1000  # This is the refresh rate for the live plot.
+saving_folder = "C:/Users/ajwgroup/Documents/Robert J Scales/Current Logger Tests/"  # Change "\" to "/".
+live_plot_refresh_time = 100  # This is the refresh rate for the live plot.
+print_values_into_console = False  # Have this set to True to print out the recorded values into the IDE console.
 # Not Recommended to Change Settings
 baud_rate = 115200  # This is the baud rate to read the serial port connected to the Arduino.
 current_logger_serial_number = None  # Input None if you do not know the serial_number of the Arduino.
+user_defined_com = None  # Best leave at None, unless it can't search for it, then try 'COM3' for the
+# front lab pc for example.
 
 
-def connect_2_arduino(serial_number=None, baud=115200):
+def connect_2_arduino(com=None, serial_number=None, baud=115200):
     # Inspired from https://stackoverflow.com/a/40531041
     # ports = list(tool_port.comports())  # serial.tools.list_ports.comports() .grep('Arduino')
-    if serial_number is None:
-        arduino_ports = list(tool_port.grep('Arduino'))
-        if not arduino_ports:
+    if com is None:
+        if serial_number is None:
+            arduino_ports = list(tool_port.grep('USB'))
+            if not arduino_ports:
+                print_list_of_ports()
+                raise IOError("No Arduino found")
+            if len(arduino_ports) > 1:
+                print_list_of_ports()
+                warn('Multiple Arduinos found - using the first')
+            arduino_port = arduino_ports[0].device  # Without the .device it didn't work!
+            out_ser = serial.Serial(arduino_port, baud)
+            print("Connected to Arduino port:" + arduino_port)
+            return out_ser
+        else:
+            for p_info in serial.tools.list_ports.comports():
+                if p_info.serial_number == serial_number:
+                    out_ser = serial.Serial(p_info.device, baud)
+                    print("Connected to Arduino port:" + p_info.device)
+                    return out_ser
             print_list_of_ports()
-            raise IOError("No Arduino found")
-        if len(arduino_ports) > 1:
-            print_list_of_ports()
-            warn('Multiple Arduinos found - using the first')
-        arduino_port = arduino_ports[0]
+            raise IOError("Could not find an arduino - is it plugged in?")
+    else:
+        arduino_port = com
         out_ser = serial.Serial(arduino_port, baud)
         print("Connected to Arduino port:" + arduino_port)
         return out_ser
-    else:
-        for p_info in serial.tools.list_ports.comports():
-            if p_info.serial_number == serial_number:
-                out_ser = serial.Serial(p_info.device, baud)
-                print("Connected to Arduino port:" + p_info.device)
-                return out_ser
-        print_list_of_ports()
-        raise IOError("Could not find an arduino - is it plugged in?")
 
 
 def print_list_of_ports():
@@ -94,55 +112,60 @@ def func_generate():
                 # The below line just adds the elapsed time into the csv file along with the Arduino data.
                 line2print = str(elapsed_time) + ',' + final_data
                 csv_file.write(line2print + "\n")  # Write data with a newline in the CSV file.
-                print(line2print)
+                if print_values_into_console:
+                    print(line2print)
+        if keyboard.is_pressed('esc'):
+            print("\nUser has pressed the 'escape' key \n ... and so data collecting has been terminated...")
+            break
 
 
 def func_live_plot():
-    def animate(i):
-        try:
-            data = pd.read_csv(fileName)
-        except Exception as er:
-            warn('Could not find automatically generated CSV file!')
-            print(er)
-            return
+    print('Started: func_live_plot...')
 
-        try:
-            values_time = data['Time (s):']
-            values_current = data['Current (mA):']
-            values_voltage = data['Load Voltage (V):']
-        except Exception as er:
-            warn('Could not load in column data from CSV file!')
-            print(er)
-            return
+    def animate(i):  # The "i" has to be left here!
+        data = pd.read_csv(fileName)
+        values_time = data['Time (s):']
+        values_current = data['Current (mA):']
+        values_voltage = data['Load Voltage (V):']
 
         plt.cla()
 
         if TF_IV == 'I vs V':
-            plt.plot(values_voltage, values_current, label='Current')
+            plt.scatter(values_voltage, values_current, label='Current')
         else:
-            plt.plot(values_time, values_current, label='Current')
+            plt.scatter(values_time, values_current, label='Current')
 
         plt.legend(loc='upper left')
         plt.tight_layout()
 
-    ani = FuncAnimation(plt.gcf(), animate, interval=live_plot_refresh_time)
+        if keyboard.is_pressed('esc'):
+            print("\nUser has pressed the 'escape' key \n ... and so Live Plot has been paused...")
+            ani.pause()
+
+    print('Started: calling animate...')
+    ani = FuncAnimation(plt.gcf(), animate, interval=live_plot_refresh_time)  # Have to keep ani in there to work!
 
     plt.tight_layout()
     plt.show()
+    print('Finished: def func_live_plot...')
 
 
 # Main Section of Code #
 print("Started: Main Section...")
 
-ser = connect_2_arduino(serial_number=current_logger_serial_number, baud=baud_rate)
+ser = connect_2_arduino(com=None, serial_number=current_logger_serial_number, baud=baud_rate)  # com='COM3'
 
 # GUI interface for selecting final mode to plot. Useful to have here so that it's added into the file name of the csv.
 print('User Input: Select mode...')
 TF_IV = easygui.buttonbox('Choose analysis mode:', 'Analysis Mode', ['I vs t', 'I vs V'])
 print(TF_IV)
 
+
 # This is the name of the CSV file which will be automatically created and saved.
-fileName = session_name + "_" + TF_IV + "_" + dt.datetime.now().strftime("%H-%M-%S") + ".csv"
+fileName_proto = session_name + "_" + TF_IV + "_" + dt.datetime.now().strftime("%H-%M-%S") + ".csv"
+data_folder = Path(saving_folder)
+fileName = data_folder / fileName_proto
+print(f'\nSaving file "{fileName_proto}"\n... in "{data_folder}"...\n')
 
 # This section initialises the CSV file with the correct headers.
 print('Started: Creating csv file...')
@@ -150,7 +173,7 @@ with open(fileName, 'w') as csv_file_1:
     headers = "Time (s):,Bus Voltage (V):,Shunt Voltage (mV):,Load Voltage (V):,Current (mA):,Power (mW):"
     csv_file_1.write(headers + "\n")  # Write data with a newline
     print(headers)
-print('Finished: Creating csv file...')
+print('Finished: Creating csv file...\n')
 
 # This defines when the code "starts" recording, and from this defines a time at which the while loop below will end at.
 codeStartTime = dt.datetime.now()
@@ -158,10 +181,11 @@ codeStartTime = dt.datetime.now()
 print('Started: Threading section...')
 
 t1 = Thread(target=func_generate)
-t2 = Thread(target=func_live_plot)
+
+print("\nHold the 'escape' key on the keyboard to stop the program but keep the plot up...\n")
 
 t1.start()
-t2.start()
+func_live_plot()
+
 
 print('Finished: End of code!')
-
